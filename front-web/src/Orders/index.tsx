06c678eb81089db1000e4ff8d08b024cc2bacf69
LucianoPierdona <lucianompj1@gmail.com>
2021-01-09 18:01:58 -0300

    created ordersummary, and helpers file

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -2,13 +2,15 @@ import React, { useEffect, useState } from 'react';
 import './styles.css';
 import StepsHeader from './StepsHeader';
 import ProductsList from './ProductsList';
-import axios from 'axios';
 import { OrderLocationData, Product } from '../types';
 import { fetchProducts } from '../api';
 import OrderLocation from './OrderLocation';
+import OrderSummary from './OrderSummary';
+import { checkIsSelected } from './helpers';
 
 const Orders = () => {
   const [products, setProducts] = useState<Product[]>([]);
+  const [selectedProducts, setSelectedProducts] = useState<Product[]>([]);
   const [orderLocation, setOrderLocation] = useState<OrderLocationData>();
 
   useEffect(() => {
@@ -17,13 +19,31 @@ const Orders = () => {
       .catch((err) => console.log(err));
   }, []);
 
+  const handleSelectProduct = (product: Product) => {
+    const isAlreadySelected = checkIsSelected(selectedProducts, product);
+
+    if (isAlreadySelected) {
+      const selected = selectedProducts.filter(
+        (item) => item.id !== product.id
+      );
+      setSelectedProducts(selected);
+    } else {
+      setSelectedProducts((previous) => [...previous, product]);
+    }
+  };
+
   return (
     <div className="orders-container">
       <StepsHeader />
-      <ProductsList products={products} />
+      <ProductsList
+        products={products}
+        onSelectProduct={handleSelectProduct}
+        selectedProducts={selectedProducts}
+      />
       <OrderLocation
         onChangeLocation={(location) => setOrderLocation(location)}
       />
+      <OrderSummary />
     </div>
   );
 };
